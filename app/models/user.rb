4f7b9a28d285f5612c9f78d0d1e05c95bdf2cd5e
JUles Saliot <julesaliot@gmail.com>
2023-02-28 16:25:10 +0000

    styled devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_many :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, presence: true
 end
