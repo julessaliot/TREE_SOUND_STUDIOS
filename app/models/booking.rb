d578cb998e3a883eca11a14b1caf89d21ada4ecd
JUles Saliot <julesaliot@gmail.com>
2023-02-28 14:44:53 +0000

    added the bookings controller

---
+++ app/models/booking.rb
@@ -0,0 +1,18 @@
+class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :studio
+
+  validates :total_price, presence: true
+  validates :booked_at, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :end_must_be_after_start, presence: true
+
+  private
+
+  def end_must_be_after_start
+    if start_time >= end_time
+      errors.add(:end_time, "must be after start time!")
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_175651) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_123221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "booked_at"
+    t.integer "total_price"
+    t.bigint "user_id", null: false
+    t.bigint "studio_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["studio_id"], name: "index_bookings_on_studio_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "studios", force: :cascade do |t|
     t.string "name"
@@ -41,5 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_175651) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "studios"
+  add_foreign_key "bookings", "users"
   add_foreign_key "studios", "users"
 end
