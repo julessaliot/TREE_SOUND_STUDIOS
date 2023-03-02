7478f2a00ed09711828c12482d44a31836021327
Tahlia McLeod <tahlia.s.mcleod@gmail.com>
2023-03-02 08:55:46 -0500

    map stuff

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,8 @@
 
 import { application } from "./application"
 
-import AudioController from "./audio_controller"
-application.register("audio", AudioController)
-
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import MapController from "./map_controller"
+application.register("map", MapController)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_165722) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_110331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_165722) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_studios_on_user_id"
   end
 
